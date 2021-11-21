c2e183dcc0493d43510c765b8ed0dd922391958f
Lizandra421583 <94806645+Lizandra421583@users.noreply.github.com>
2021-11-21 18:42:52 -0300

    Create teste01.java

---
+++ teste01.java
@@ -0,0 +1,11 @@
+public class Helloword{
+  \\Declaracao de variaveis
+  private String saudacao;
+  
+  \\Construtor de classe
+    public Helloword (String n){
+    saudacao = n;
+    System.out.print(saudacao);
+  }
+  \\Teste Commit Git
+  }
